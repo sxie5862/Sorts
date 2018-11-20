60e98d6279bfe5ee787c24634449a9f43b16e01a
sxie5862 <woshinidienmsl@gmail.com>
2018-11-20 14:16:56 -0500

    Add .gitignore file.

--- src/Sorts.java
+++ src/Sorts.java
@@ -25,4 +25,31 @@ public class Sorts {
         }
         System.out.println();
     }
+
+    public static void ascendingSort(String[] arr)
+    {
+        for(int i = 0; i < arr.length-1; i++)
+        {
+            int lowposition = i;
+
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[j].compareTo(arr[lowposition]) < 0)
+                    lowposition = j;
+            }
+
+            String temp = arr[i];
+            arr[i] = arr[lowposition];
+            arr[lowposition] = temp;
+        }
+    }
+
+    public static void printarr(String[] arr)
+    {
+        for(String num : arr)
+        {
+            System.out.print(num +" ");
+        }
+        System.out.println();
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,12 @@ public class Test {
     public static void main(String[] args)
     {
         int[] mylist =  {1,2,-88,21314,124211152};
+        String[] myarray =  {"aa","ba","bc","ad","ee","asdasf","sdag","ggh","eei"};
         Sorts.printarr(mylist);
         Sorts.ascendingSort(mylist);
         Sorts.printarr(mylist);
+        Sorts.printarr(myarray);
+        Sorts.ascendingSort(myarray);
+        Sorts.printarr(myarray);
     }
 }
